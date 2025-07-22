d76f8e78637bfae319846730b13ebbf3a9d91128
Harli Laçej <59536016+harli-lacej@users.noreply.github.com>
2025-07-22 17:30:05 +0200

    Add files via upload

---
+++ realestate-frontend/views/LoginView.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="row login-box bg-white d-flex w-100">
+    <div class="col-md-6 d-none d-md-block p-0 login-image-wrapper">
+      <figure>
+      <img src="/login-banner.png" alt="Login Image" class="login-image" />
+      </figure>
+    </div>
+    <div class="col-md-6 col-12 p-5 d-flex align-items-center justify-content-center">
+      <div class="w-100" style="max-width: 400px;">
+        <h2 class="mb-4 text-center form-title">Admin login</h2>
+        <form @submit.prevent="handleLogin">
+          <div class="mb-3">
+            <label for="username" class="form-label">Username:</label>
+            <input
+              v-model="username"
+              type="text"
+              class="form-control"
+              id="username"
+              placeholder="Enter username"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password:</label>
+            <input
+              v-model="password"
+              type="password"
+              class="form-control"
+              id="password"
+              placeholder="Enter password"
+              required
+            />
+          </div>
+          <button type="submit" class="btn btn-primary w-100">Login</button>
+        </form>
+        <div v-if="errorMessage" class="alert alert-danger mt-3">{{ "Hmm... " + errorMessage +". Try again!" }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Navbar from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+ return {
+    username: '',
+    password: '',
+    errorMessage: '',
+    redirectTo: '/dashboard'
+  };
+  },
+  mounted() {
+    document.title = 'RealDeal | Login';
+  },
+  methods: {
+    async handleLogin() {
+        try {
+    const res = await axios.post('https://real-estate-app-4vp2.onrender.com/users/login', {
+      username: this.username,
+      password: this.password
+    });
+
+    localStorage.setItem('isLoggedIn', 'true');
+    this.$router.push(this.redirectTo);
+  } catch (error) {
+    this.errorMessage = error.response?.data?.message || 'Login failed';
+  }
+    }
+  },
+  created() {
+  const redirectParam = this.$route.query.redirect;
+  if (redirectParam) {
+    this.redirectTo = redirectParam;
+  }
+}
+};
+</script>
---
+++ realestate-frontend/views/PrivacyView.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+    <div class="free-space"></div>
+    <div class="free-space"></div>
+    <div class="container text-center">
+        <div class="row align-items-center">
+          <div class="col">
+          </div>
+          <div class="col">
+            <h1 class="title">Privacy policy</h1>
+          </div>
+          <div class="col">
+          </div>
+        </div>
+      </div>
+      <div class="container text-left">
+        <div class="row align-items-center">
+          <div class="col">
+            <div class="free-space"></div>
+                <p class="fs-6"> Real Deal - Real Estate respects your privacy and security and is committed to protecting your personal data. 
+                This privacy policy aims to inform you on how we manage your personal data, about your data and privacy rights, 
+                along with how you are protected.
+                Please read this policy carefully before using the page because it explains how we process personal data and how we use cookies.
+                By using the site you are accepting this policy.
+                </p>
+                <h6 class="text-orange">OUR PRIVACY POLICY</h6>
+                <p class="fs-6">This Privacy Policy explains how we use the information we collect about you, how you can instruct us if you 
+                    prefer to limit the use of that information and procedures that we have in place to safeguard your privacy.
+                </p>
+                <h6 class="text-orange">DATA COLLECTED</h6>
+                <p class="fs-6">While using Our Service, we may ask you to provide us with certain personally identifiable information that can
+                     be used to contact or identify you. That information may include, but is not limited to:
+                    <ul>
+                    <li>Email address</li>
+                
+                    <li>First name and last name</li>
+                
+                    <li>Phone number</li>
+                
+                    <li>Usage Data</li>
+                    </ul>
+                </p>
+                
+                <h6 class="text-orange">USAGE DATA</h6>
+                <p class="fs-6">Usage Data is collected automatically when using this website.Usage Data may include information such as your
+                device's Internet Protocol address (IP address), browser type, browser version, the pages of our website that you visit, the time
+                and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.
+                When You access this website by or through a mobile device, we may collect certain information automatically, including, but not
+                limited to, the type of mobile device you use, your mobile device unique ID, the IP address of your mobile device, your mobile 
+                operating system, the type of mobile Internet browser you use, unique device identifiers and other diagnostic data.
+                
+                We may also collect information that your browser sends whenever you visit our website or when you access the website by or 
+                through a mobile device.</p>
+                
+                <h6 class="text-orange">USE OF YOUR DATA</h6>
+                We use your data for the following purposes:
+                <ul>
+                    <li>To provide and maintain our website: including to monitor the usage of our Service.</li>
+                    <li>To contact you: To contact you by email, telephone calls, SMS, or other equivalent forms of electronic communication, 
+                        such as a mobile application's push notifications regarding updates or informative communications related to the 
+                        functionalities, products or contracted services, including the security updates, when necessary or reasonable for 
+                        their implementation.</li>
+                
+                    <li>To contact you: To contact you by email, telephone calls, SMS, or other equivalent forms of electronic communication, 
+                            To manage your requests/consultations: To attend and manage your requests and consultations asked to us.</li>
+                </ul>
+                
+                    We may share Your personal information in the following situations:
+                
+                <ul>
+                    <li>With Service Providers: We may share your personal information with service providers to monitor and analyze
+                        the use of our website, to contact you.
+                
+                    For business transfers: We may share or transfer your personal information in connection with other service providers
+                    and real estate companies.</li>
+                </ul>
+                
+                <h6 class="text-orange">YOUR CONSENT</h6>
+                <p class="fs-6">By submitting your information, you consent to the use of that information as set in this Policy.
+                    If we change our Privacy Policy we will post the changes on this page, and may place notices on other pages of the website,
+                    so that you may be aware of the information we collect and how we use it at all times. We will also e-mail you should we 
+                    make any changes so that you may consent to our use of your information in that way. Continued use of the service will 
+                    signify that you agree to any changes.</p>
+                
+                <p class="fs-6 text-orange">Last updated: 22th April 2025</p>
+          </div>
+        </div>
+      </div>
+    <div class="free-space"></div>
+    <div class="free-space"></div>
+ </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+    mounted() {
+    document.title = "RealDeal | Privacy";
+  },
+};
+</script>
