db2bd4b02ceb1d8a893d95be3eb9a5f430031fd2
Harli Laçej <59536016+harli-lacej@users.noreply.github.com>
2025-07-23 17:27:17 +0200

    Update socket.js

--- realestate-frontend/src/socket.js
+++ realestate-frontend/src/socket.js
@@ -1,4 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://real-estate-app-4vp2.onrender.com/');
+const socket = io('https://real-estate-app-4vp2.onrender.com', {
+  transports: ['websocket'],
+  withCredentials: true,
+});
+
 export default socket;
